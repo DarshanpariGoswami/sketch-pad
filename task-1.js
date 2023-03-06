88b99f6d17d7e7f8b819542fa685869369afa563
DarshanpariGoswami <124126426+DarshanpariGoswami@users.noreply.github.com>
2023-03-06 17:05:37 +0530

    Add files via upload

---
+++ task-1.js
@@ -0,0 +1,18 @@
+const grid = document.querySelector(".gridContainer");
+const Input = document.getElementById("userinput");
+const resetButton = document.querySelector(".reset");
+
+createGrid = () => {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
+};
+
+updateGrid = () => {
+  grid.innerHTML = "";
+  grid.style.setProperty(
+    "grid-template-columns",
+    `repeat(${input.value}, 2fr)`
+  );
