4969c555a880b8b2e1560b8070ce65427bf13597
DarshanpariGoswami <124126426+DarshanpariGoswami@users.noreply.github.com>
2023-03-07 18:55:21 +0530

    Add files via upload

--- task-1.js
+++ task-1.js
@@ -1,9 +1,9 @@
-const grid = document.querySelector(".gridContainer");
-const Input = document.getElementById("userinput");
+const grid = document.querySelector(".box");
+const userInput = document.getElementById("userinpute");
 const resetButton = document.querySelector(".reset");
 
-createGrid = () => {
-  for (let i = 0; i < 256; i++) {
+createGrid = (userInput) => {
+  for (let i = 0; i < userInput; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
     grid.appendChild(div);
@@ -14,5 +14,52 @@ updateGrid = () => {
   grid.innerHTML = "";
   grid.style.setProperty(
     "grid-template-columns",
-    `repeat(${input.value}, 2fr)`
+    `repeat(${userInput.value}, 2fr)`
   );
+  grid.style.setProperty(
+    "grid-template-rows",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  for (let i = 0; i < userInput.value * userInput.value; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
+  console.log(userInput.value);
+};
+
+function coworker() {
+  userbutton.addEventListener("click", function () {
+    let clorox = document.getElementById("colorizer");
+    let color = clorox.value;
+    return color;
+  });
+}
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function (event) {
+  event.target.classList.replace("square", "color");
+});
+/*
+var gridItems = document.querySelectorAll(".grid");
+let clorox = document.getElementById("colorx");
+gridItems.forEach(function (item) {
+  item.addEventListener("mouseover", function () {
+    this.style.backgroundColor = clorox.value; // default black on load
+  });
+});
+var gridItems = document.querySelectorAll(".grid");
+gridItems.forEach(function (item) {
+  item.addEventListener("mouseover", function () {
+    this.style.backgroundColor = clorox.value;
+  });
+});*/
+
+userInput.addEventListener("change", updateGrid);
+
+resetButton.addEventListener("click", function () {
+  grid.innerHTML = "";
+  userInput.value = "";
+  grid.style.setProperty("grid-template-columns", `repeat(25, 2fr)`);
+  grid.style.setProperty("grid-template-rows", `repeat(25, 2fr)`);
+  createGrid();
+});
